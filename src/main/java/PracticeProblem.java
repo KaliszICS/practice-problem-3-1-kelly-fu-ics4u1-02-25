4f30ea9ff5a1ced1a29c8e18d637b1a3cfb25b87
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-04-07 23:00:21 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,32 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int sumOfDigits(int x){
+	String temp = Integer.toString(x);
+	if (x<0){
+	 x = x*(-1);
 	}
+   
+	if (temp.length() ==1){
+	 return x;
+	}
+	else {
+	 return (x%10) + sumOfDigits(x/10);
+	}
+	}
+   
+   
+	public static int factorial(int n){
+	 if (n<0){
+	  return 0;
+	 }
+	 else if (n>0){
+	  return n * factorial(n-1);
+	 }
+	 else {
+	  return 1;
+	 }
+	}
+	   
+   
+   }
 
-	
-
-}
